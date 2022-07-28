0adc0f829213eda0605350c2608058475d4e8f63
santlunaschiavi <santlunaschiavi@gmail.com>
2022-07-28 19:50:10 -0300

    agregando register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,19 @@ export class AppComponent implements OnInit{
   public identity:any;
   public user: User;
   public errorMessage:any;
+  public user_register:any;
+  public alertRegister:any;
   
 
   constructor(
     private _userService:UserService
   ){
   this.user=new User('','','','','ROLE_USER','','');
+  this.user_register=new User('','','','ROLE_USER','','','');
 }
 ngOnInit(){
-  this.identity=this._userService.getIdentity();
-  this.token=this.token._userService.getToken();
+  this.identity=this?._userService.getIdentity();
+  this.token=this.token?._userService.getToken();
   console.log(this.identity);
   console.log(this.token);
   
@@ -62,6 +65,7 @@ public onSubmit(){
             else{
               //*Crear elemento en el localstorage para tener token disponible
               localStorage.setItem('token',token);
+              this.user_register=new User('','','','ROLE_USER','','','');
               console.log(token);
               console.log(identity);
               
@@ -88,6 +92,32 @@ public onSubmit(){
       localStorage.clear();
       this.identity=null;
       this.token=null;
+    }
+    onSubmitRegister(){
+      console.log(this.user_register);
+      this._userService.register(this.user_register).subscribe(
+        response=>{
+          let user=response.user;
+          this.user_register=user;
+          if(!user._id){
+            this.alertRegister('Error al registrarse');
+          }else{
+            this.alertRegister=('El registro se realizo bien usa el mail '+ this.user_register.email)
+            this.user_register=new User('','','','ROLE_USER','','','');
+
+          }
+        }
+        ,
+        error=>{
+          var errorMessage=<any>error;
+            if (errorMessage!=null){
+              var body=JSON.parse(error._body)
+              this.alertRegister=body.message;
+              console.log(error)
+            }
+        }
+      )
+      
     }
     
 }
